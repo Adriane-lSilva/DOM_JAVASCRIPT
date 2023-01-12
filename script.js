6e9804572d3bbcad8db3119055d043948b32ecab
Adriane-lSilva <122410076+Adriane-lSilva@users.noreply.github.com>
2023-01-11 21:17:31 -0300

    Aula 5
    Função copiar

---
+++ script.js
@@ -0,0 +1,35 @@
+var entrada = document.getElementById("entrada");
+var botaoEnviar = document.getElementById("enviar");
+var saida = document.getElementById("saida");
+
+function copiarEntrada(){
+saida.textContent = entrada.value;
+}
+
+botaoEnviar .addEventListener("click", copiarEntrada);
+botaoEnviar .addEventListener("click", copiarEntrada);
+
+
+function minhaFuncao(nome){
+alert(minhaVariavel);
+minhaVariavel = "Valor definido";
+alert(minhaVariavel);
+minhaVariavel = 150;
+alert(minhaVariavel);
+
+if(varBooleana){
+alert("Condição verdadeira");
+}
+else{
+alert("Condição falsa");
+}
+*/ 
+    /* alert("alert 1");
+ alert("nome")
+}
+
+minhaFuncao("Adriane");
+
+minhaFuncao("Silva");
+
+ minhaFuncao("Lopes");*/ 
